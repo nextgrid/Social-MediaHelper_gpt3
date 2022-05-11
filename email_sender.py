7b70e381004c83053373e56c0c310c859b7dcf8b
jmisilo <jmisilo@interia.pl>
2022-05-11 20:16:56 +0200

    Fix issue with incomprehensible variable names

--- email_sender.py
+++ email_sender.py
@@ -5,14 +5,14 @@ from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-def send_email(password, from_email, to_email, f):
-    t = time.time()
+def send_email(password, from_email, to_email, attached_file):
+    time_ = time.time()
     sender_address = from_email
     receiver_address = to_email
     message = MIMEMultipart("alternative")
     message['from'] = sender_address
     message['to'] = receiver_address
-    message['subject'] = f'art-{t}'
+    message['subject'] = f'art-{time_}'
 
     mail_content = f'''Hello {message['to']},
     the email contains the results of the experiment as an attachment.
@@ -22,13 +22,13 @@ def send_email(password, from_email, to_email, f):
     message.attach(MIMEText(mail_content, 'plain'))
     
     try:
-        with open(f, 'rb') as attachment:
+        with open(attached_file, 'rb') as attachment:
             p = MIMEApplication(attachment.read(), _subtype='pdf')
-            p.add_header('Content-Disposition', 'attachment', filename=f'art-{t}.pdf')
+            p.add_header('Content-Disposition', 'attachment', filename=f'art-{time_}.pdf')
             message.attach(p)
 
-    except Exception as e:
-        print(str(e))
+    except Exception as err:
+        print(str(err))
 
     make_context = ssl.create_default_context()
     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=make_context) as server:
